70066dc0fe8d333418c0c31d4c6c9205d3154dac
Sam Bhandari <sambhandari2021@yahooe.com>
2022-10-20 22:09:26 -0400

    made seperate file greeting.js

--- greeting.js
+++ greeting.js
@@ -1,11 +1,37 @@
 // function showGreetingInConsole() {
 //     let message = "Hello world";
 //     console.log(message);
-    
+
 // }
 // // showGreetingInConsole();
 
 
+// window.onload = init;
+
 window.onload = init;
+
+let myGreetBtn = document.getElementById('greetBtn');
+myGreetBtn.addEventListener('click', buttonClicked);
+// console.log(myGreetBtn);
+
+
+
+function init() {
+buttonClicked();
+}
+
+
+
+function buttonClicked() {
+// console.log('yes');
+let nameField = document.getElementById('nameField');
+// console.log(nameField);
+let name = nameField.value;
+// console.log(name);
+let greeting = `Hello ${name}`;
+window.alert(greeting);
+
+
+}
 
 
